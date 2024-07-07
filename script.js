6d3313d3dc56b1670c91e83c403d58e4c27ab8dd
vishwa-radhya <vishuvishwa735@gmail.com>
2024-07-07 12:24:30 +0530

    mobile responsive

--- script.js
+++ script.js
@@ -5,13 +5,7 @@ let isIconsOpen = false;
 
 burger.addEventListener('click',()=>{
     isIconsOpen =!isIconsOpen;
-    if(window.innerWidth>600){
-    iconContainer.style.width = isIconsOpen ? '20%' : '0%';
-    }else if(window.innerWidth<600 && window.innerWidth>400){
-        iconContainer.style.width = isIconsOpen ? '30%' : '0%';
-    }else if(window.innerWidth<380){
-        iconContainer.style.width = isIconsOpen ? '40%' : '0%';
-    }
+    iconContainer.style.width = isIconsOpen ? '100%' : '0%';
 })
 document.getElementById('gh-btn').addEventListener('click',()=>{
     window.location.href='https://github.com/vishwa-radhya/';
